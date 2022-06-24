dd86d71e908f217ab19643d12c374e4b93320e24
Raul Corral <raulcorral20@gmail.com>
2022-06-24 00:47:49 -0500

    Correccion error con fetch

--- main.js
+++ main.js
@@ -78,7 +78,7 @@ const menuValidation = () =>{
     status === true ? (setloggedMenu(), removeLoginMenu())
     : console.log("cuenta no logeada")
   }).catch((error) => {
-    console.log(error)
+    console.log(error, "Cuenta no logeada")
   })
 } 
 window.addEventListener('load',menuValidation)
